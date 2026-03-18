64315009d4cb9fbb4ec44bc2282dda9a946adea1
hotrong14 <trongarsenal08@gmail.com>
2026-03-18 09:34:04 +0700

    feat:payment

---
+++ backend/order_table/src/main/java/restaurant/project/order_table/entity/enums/PaymentStatus.java
@@ -0,0 +1,8 @@
+package restaurant.project.order_table.entity.enums;
+
+public enum PaymentStatus {
+    PENDING,    // Đang chờ xử lý
+    SUCCESS,    // Thành công
+    FAILED      // Thất bại
+}
+
