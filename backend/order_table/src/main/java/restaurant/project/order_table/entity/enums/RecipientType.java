3f6dc28ef4bbfd4b3aeae0d480a06d32bf9ac2a8
TruongDx2004 <doanxuantruong2110@gmail.com>
2026-04-09 17:26:37 +0700

    hotfix: resolve production java issue

--- backend/order_table/src/main/java/restaurant/project/order_table/entity/enums/RecipientType.java
+++
@@ -1,8 +0,0 @@
-package restaurant.project.order_table.entity.enums;
-
-public enum RecipientType {
-    USER,     // Nhân viên
-    CUSTOMER, // Khách hàng
-    ROLE,     // Theo vai trò
-    ALL       // Broadcast toàn bộ
-}
--- backend/order_table/src/main/java/restaurant/project/order_table/repository/RestaurantConfigRepository.java
+++
@@ -1,9 +0,0 @@
-package restaurant.project.order_table.repository;
-
-import restaurant.project.order_table.entity.RestaurantConfigEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RestaurantConfigRepository extends JpaRepository<RestaurantConfigEntity, Long> {
-}
